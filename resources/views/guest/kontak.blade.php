6066abb7951d2d6c63a8778932e5def25f6afc10
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2025-09-11 08:04:23 +0700

    kontak

--- resources/views/guest/kontak.blade.php
+++ resources/views/guest/kontak.blade.php
@@ -15,11 +15,11 @@
     <!-- End page header -->
 
     <!-- property area -->
-    <div class="content-area recent-property padding-top-40" style="background-color: #FFF;">
+    <div class="content-area recent-property padding-top-40 parallax-bg">
         <div class="container">
             <div class="row">
                 <div class="col-md-8 col-md-offset-2">
-                    <div class="" id="contact1">
+                    <div class="text-black" id="contact1">
                         <div class="row">
                             <div class="col-sm-4">
                                 <h3><i class="fa fa-map-marker"></i> {{ __('global.Alamat') }}</h3>
@@ -32,13 +32,15 @@
                             <!-- /.col-sm-4 -->
                             <div class="col-sm-4">
                                 <h3><i class="fa fa-phone"></i>{{ __('global.PusatPanggilan') }}</h3>
-                                <p class="text-muted">Kami sarankan Anda menggunakan bentuk komunikasi elektronik.</p>
+                                <p class="text-black text-muted">Kami sarankan Anda menggunakan bentuk komunikasi
+                                    elektronik.</p>
                                 <p><strong> (62-21) 3846811, 3500368</strong></p>
                             </div>
                             <!-- /.col-sm-4 -->
                             <div class="col-sm-4">
                                 <h3><i class="fa fa-envelope"></i>{{ __('global.DukunganElektronik') }}</h3>
-                                <p class="text-muted">Jangan ragu untuk menulis email kepada kami atau menggunakan sistem
+                                <p class=" text-blacktext-muted">Jangan ragu untuk menulis email kepada kami atau
+                                    menggunakan sistem
                                     tiket elektronik kami.</p>
                                 <ul>
                                     <li><strong><a href="mailto:">gapkindo.pusat@gmail.com,</a></strong> </li>
@@ -52,46 +54,6 @@
                         <div id="map" style="width:100%; height:400px;"></div>
 
                         <hr>
-                        <h2>Contact form</h2>
-                        <form>
-                            <div class="row">
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="firstname">Firstname</label>
-                                        <input type="text" class="form-control" id="firstname">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="lastname">Lastname</label>
-                                        <input type="text" class="form-control" id="lastname">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="email">Email</label>
-                                        <input type="text" class="form-control" id="email">
-                                    </div>
-                                </div>
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label for="subject">Subject</label>
-                                        <input type="text" class="form-control" id="subject">
-                                    </div>
-                                </div>
-                                <div class="col-sm-12">
-                                    <div class="form-group">
-                                        <label for="message">Message</label>
-                                        <textarea id="message" class="form-control"></textarea>
-                                    </div>
-                                </div>
-                                <div class="col-sm-12 text-center">
-                                    <button type="submit" class="btn btn-primary"><i class="fa fa-envelope-o"></i> Send
-                                        message</button>
-                                </div>
-                            </div>
-                            <!-- /.row -->
-                        </form>
                     </div>
                 </div>
             </div>
