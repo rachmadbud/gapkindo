e477083c762f8210dc2e4d620b14da5c6c55eedc
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2025-09-15 13:23:04 +0700

    10 slide foto

--- resources/views/guest/partials/slider.blade.php
+++ resources/views/guest/partials/slider.blade.php
@@ -7,36 +7,46 @@
 
             <div class="item"><img src="{{ asset('guest/assets/img/slide1/1634091544930.jpg') }}" alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/DS23123.jpeg') }}" alt="The Last of us">
+
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/2.10.04_ba59a1d0.jpg') }}"
+                    alt="GTA V"></div>
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.01_5b38efa9.jpg') }}"
+                    alt="GTA V"></div>
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.01_56766b74.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/DSC04304.JPG') }}" alt="GTA V">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.03_8edbfd7f.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/1634091544930.jpg') }}" alt="GTA V">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.05_51448337.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/DS23123.jpeg') }}" alt="The Last of us">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.06_63908e39.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/DSC04304.JPG') }}" alt="GTA V">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.06_31123138.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/1634091544930.jpg') }}" alt="GTA V">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.06_ca8e59c7.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/DS23123.jpeg') }}" alt="The Last of us">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.07_878b5975.jpg') }}"
+                    alt="GTA V">
             </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/DSC04304.JPG') }}" alt="GTA V">
-            </div>
-            <div class="item"><img src="{{ asset('guest/assets/img/slide1/1634091544930.jpg') }}" alt="GTA V">
+            <div class="item"><img src="{{ asset('guest/assets/img/slide1/slide10/12.10.07_df410632.jpg') }}"
+                    alt="GTA V">
             </div>
 
         </div>
-    </div>
-    <div class="slider-content">
-        <div class="row">
-            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
+        <div class="slider-content">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1 col-sm-12">
 
-                <blockquote><cite><a href="#">{{ __('global.h1') }}</a></cite>
-                    <p>{{ trans('global.p') }}
-                    </p>
-                </blockquote>
+                    <blockquote><cite><a href="#">{{ __('global.h1') }}</a></cite>
+                        <p>{{ trans('global.p') }}
+                        </p>
+                    </blockquote>
+                </div>
             </div>
         </div>
     </div>
-</div>
