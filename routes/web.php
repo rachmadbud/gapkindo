8054e966bd3ba74bca775fc16889669cd54fde99
rachmadbud <rachmad.16421044@student.ubl.ac.id>
2026-01-26 16:11:34 +0700

    publikasi1 web.php

--- routes/web.php
+++ routes/web.php
@@ -100,7 +100,7 @@ Route::get('/excel', function () {
   return view('guest.excel');
 })->name('');
 
-Route::get('/publikasi1', 'PublikasiController@index')->name('publikasi');
+Route::get('/publikasi1', 'PublikasiController@index')->name('publikasi1');
 
 
 Auth::routes();
